ab35fb929dd74fc864c38ada66a0947eaca733e6
Jeffrey Dorfman <dorfman2@buffalo.edu>
2019-10-21 11:48:40 -0400

    switched to code that allows forward and backward movement

--- stepper.py
+++ stepper.py
@@ -3,30 +3,51 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
-control_pins = [7,11,13,15]
+coil_A_1_pin = 7    # IN1
+coil_A_2_pin = 11   # IN2
+coil_B_1_pin = 13   # IN3
+coil_B_2_pin = 15   # IN4
 
-for pin in control_pins:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, 0)
+StepCount = 8
+Seq = range(0, StepCount)
+Seq[0] = [1,0,0,0]
+Seq[1] = [1,1,0,0]
+Seq[2] = [0,1,0,0]
+Seq[3] = [0,1,1,0]
+Seq[4] = [0,0,1,0]
+Seq[5] = [0,0,1,1]
+Seq[6] = [0,0,0,1]
+Seq[7] = [1,0,0,1]
 
-halfstep_seq = [
-    [1,0,0,0],
-    [1,1,0,0],
-    [0,1,0,0],
-    [0,1,1,0],
-    [0,0,1,0],
-    [0,0,1,1],
-    [0,0,0,1],
-    [1,0,0,1]
-]
+GPIO.setup(coil_A_1_pin, GPIO.OUT)
+GPIO.setup(coil_A_2_pin, GPIO.OUT)
+GPIO.setup(coil_B_1_pin, GPIO.OUT)
+GPIO.setup(coil_B_2_pin, GPIO.OUT)
 
-for i in range(512):
-    for halfstep in range(8):
-        for pin in range(4):
-            GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-        time.sleep(0.001)
+def setStep(w1, w2, w3, w4):
+    GPIO.output(coil_A_1_pin, w1)
+    GPIO.output(coil_A_2_pin, w2)
+    GPIO.output(coil_B_1_pin, w3)
+    GPIO.output(coil_B_2_pin, w4)
 
+def forward(delay, steps):
+    for i in range(steps):
+        for j in range(StepCount):
+            setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
+            time.sleep(delay)
 
+def backwards(delay, steps):
+    for i in range(steps):
+        for j in reversed(range(StepCount)):
+            setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
+            time.sleep(delay)
 
-GPIO.cleanup()
+if __name__ == '__main__':
+    while True:
+        delay = raw_input("Time Delay (ms)?")
+        steps = raw_input("How many steps forward?")
+        forward(int(delay) / 1000.0, int(steps))
+        steps = raw_input("How many steps backwards?")
+        backwards(int(delay) / 1000.0, int(steps))
