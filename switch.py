09fc63a20af21839ab99079d12683e31450e8a66
Jeffrey Dorfman <dorfman2@buffalo.edu>
2019-10-23 22:31:25 -0400

    removed old test files

--- switch.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)   #Enable switch and pull up resistors
-while True:
-    input_state = GPIO.input(12)    #Read and store value of switch to var
-    if input_state == False:
-        print('button pressed')
-        time.sleep(0.3)
