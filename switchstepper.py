7749ec4562efe7bc1e7438c733d646b6113e1f7b
Jeffrey Dorfman <dorfman2@buffalo.edu>
2019-10-30 00:59:52 -0400

    asyncio recieves queue and executes list

--- switchstepper.py
+++ switchstepper.py
@@ -22,17 +22,17 @@ pan = RpiMotorLib.BYJMotor('Pan', '28BYJ')
 
 # Functions
 
-async def move_up():
-    tilt.motor_run(tilt_gpio_pins,.001,50,False,False,"half", .05)
+async def move_up(delay, step):
+    tilt.motor_run(tilt_gpio_pins, float(delay), int(step), True, False,"half", .05)
 
-async def move_down():
-    tilt.motor_run(tilt_gpio_pins,.001,50,True,False,"half", .05)
+async def move_down(delay, step):
+    tilt.motor_run(tilt_gpio_pins, float(delay), int(step), False, False,"half", .05)
 
-async def move_left():
-    pan.motor_run(pan_gpio_pins,.001,50,False,False,"half", .05)
+async def move_left(delay, step):
+    pan.motor_run(pan_gpio_pins, float(delay), int(step), True, False,"half", .05)
 
-async def move_right():
-    pan.motor_run(pan_gpio_pins,.001,50,True,False,"half", .05)
+async def move_right(delay, step):
+    pan.motor_run(pan_gpio_pins, float(delay), int(step), False, False,"half", .05)
 
 
 # Script
@@ -43,18 +43,42 @@ async def main():
 
 
         direction = input('Direction? ')
+
+        if direction == 'exit':
+            GPIO.cleanup()
+            exit()
+
+
+        speed = input('Speed (fast/slow)? ')
+        steps = input('Steps? ')
+
+        if speed == 'fast':
+            speed = 0.001
+        elif speed == 'slow':
+            speed = 0.01
+        else:
+            speed = 0.1
         #steps = input('Steps? ')
 
 
-        if direction == 'up':
-            task = asyncio.create_task(move_up())
-        elif direction == 'down':
-            task = asyncio.create_task(move_down())
 
-        if direction == 'left':
-            task = asyncio.create_task(move_left())
+        asyncio.create_task(move_up(0.001, 50))
+        asyncio.create_task(move_down(0.001, 50))
+        asyncio.create_task(move_left(0.001, 50))
+        asyncio.create_task(move_right(0.001, 50))
+
+        if direction == 'up':
+            task = asyncio.create_task(move_up(speed, steps))
+
+        elif direction == 'down':
+            task = asyncio.create_task(move_down(speed, steps))
+
+        elif direction == 'left':
+            task = asyncio.create_task(move_left(speed, steps))
+
         elif direction == 'right':
-            task = asyncio.create_task(move_right())
+            task = asyncio.create_task(move_right(speed, steps))
+        
 
         
 
